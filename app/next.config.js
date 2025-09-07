7fbbdc47e2206b357f78300a00e5bd93be792953
mokhantsoofentse2-debug <mokhantsoofentse2@gmail.com>
2025-09-07 10:40:19 +0200

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
   images: {
     unoptimized: true
   }
-  // REMOVE distDir and assetPrefix - they cause issues with static export
 }
 
 module.exports = nextConfig

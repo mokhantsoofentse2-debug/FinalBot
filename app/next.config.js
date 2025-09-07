0331b333095aca578b55c83e6dbd8c4a856dfbf2
mokhantsoofentse2-debug <mokhantsoofentse2@gmail.com>
2025-09-07 08:50:28 +0200

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  // Add these for safety
+  distDir: 'out',
+  assetPrefix: '.'
 }
 
 module.exports = nextConfig
